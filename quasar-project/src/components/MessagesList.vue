dd485e1d04d46a6f24296ea3e0e7dd0b5928c466
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2025-12-14 10:35:41 +0100

    Nickname highlighting

--- quasar-project/src/components/MessagesList.vue
+++ quasar-project/src/components/MessagesList.vue
@@ -23,6 +23,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { ChannelMessage } from 'src/models'
+import vHighlightMention from '../utils/highlight-mention'
 
 defineProps<{
     messages: ChannelMessage[]
