116679849e6038c0de75667d01db254a7a0a2058
Amal Akhmadinurov <amal.akhmadinurov@gmail.com>
2025-12-14 13:19:09 +0100

    small notification bug fix

--- quasar-project/src/App.vue
+++ quasar-project/src/App.vue
@@ -8,13 +8,14 @@ import {watch} from 'vue';
 import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
-watch(() => $q.appVisible, val => {
+watch( () => $q.appVisible, async val => {
     console.log(val ? 'App became visible' : 'App went in the background');
+    await navigator.serviceWorker.ready;
     navigator.serviceWorker.controller?.postMessage({type:"app_visibility",data:val});
 })
 
 navigator.serviceWorker.addEventListener("message", event => {
-
+  
   const message = event.data;
   $q.notify({
     type: 'info',
